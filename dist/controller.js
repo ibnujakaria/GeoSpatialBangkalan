920835a28139a3dac4f31e8dd30c2b25a2b6e2f8
Nurul Huda <hudasanca@gmail.com>
2016-12-13 11:26:23 +0700

    mengubah tampilan

---
+++ dist/controller.js
@@ -0,0 +1,24 @@
+addListeners()
+
+function addListeners() {
+	$('.menu .item').on('click', onHomeMenuClick)
+}
+
+function onHomeMenuClick() {
+	hideMenuUtama()
+	geserPetaKeKanan()
+}
+
+function hideMenuUtama() {
+	$('.cover.blurred').fadeOut(500)
+	$('.menu-utama').fadeOut(500)
+}
+
+function geserPetaKeKanan() {
+	$('#map').animate({
+		top: '80px',
+		left: '360px',
+		bottom: '20px',
+		right: '20px'
+	})
+}
