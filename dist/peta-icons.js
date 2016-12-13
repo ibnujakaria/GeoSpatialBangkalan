cf6f3a1c7e27d1d6c0121b9d1a5ce937275bb8e5
Nurul Huda <hudasanca@gmail.com>
2016-12-13 11:56:29 +0700

    mecah-mecah peta.js jadi beberapa file

---
+++ dist/peta-icons.js
@@ -0,0 +1,92 @@
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 81,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>, Modified © <a href="http://elcicko.web.id">ElCickoBlog</a>'
+}).addTo(map);
+
+var masjidIcon = new L.icon({
+  iconUrl: 'https://masjidabubakarcawang.files.wordpress.com/2015/04/abu-bakar.png',
+  shadowUrl: 'https://masjidabubakarcawang.files.wordpress.com/2015/04/abu-bakar.png',
+
+  iconSize:     [20, 20], // size of the icon
+  shadowSize:   [20, 20], // size of the shadow
+  iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+  shadowAnchor: [10, 10],  // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var restoranIcon = new L.icon({
+  iconUrl: 'http://dining1.ut.ac.ir/res?id=11607&dl=false',
+  shadowUrl: 'http://dining1.ut.ac.ir/res?id=11607&dl=false',
+
+  iconSize:     [20, 20], // size of the icon
+  shadowSize:   [20, 20], // size of the shadow
+  iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+  shadowAnchor: [10, 10],  // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var bankIcon = new L.icon({
+  iconUrl: 'http://www.soloptik.dk/images/payment/bank-transfer.png',
+  shadowUrl: 'http://www.soloptik.dk/images/payment/bank-transfer.png',
+
+  iconSize:     [20, 20], // size of the icon
+  shadowSize:   [20, 20], // size of the shadow
+  iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+  shadowAnchor: [10, 10],  // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var sekolahIcon = new L.icon({
+  iconUrl: 'http://zyk.ouchn.edu.cn/images/item/apic06.png',
+  shadowUrl: 'http://zyk.ouchn.edu.cn/images/item/apic06.png',
+
+  iconSize:     [20, 20], // size of the icon
+  shadowSize:   [20, 20], // size of the shadow
+  iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+  shadowAnchor: [10, 10],  // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var mallIcon = new L.icon({
+  iconUrl: 'http://vignette2.wikia.nocookie.net/cityville/images/6/67/Famous_Mall_Level_3.png/revision/latest?cb=20120626024128',
+  shadowUrl: 'http://vignette2.wikia.nocookie.net/cityville/images/6/67/Famous_Mall_Level_3.png/revision/latest?cb=20120626024128',
+
+  iconSize:     [20, 20], // size of the icon
+  shadowSize:   [20, 20], // size of the shadow
+  iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+  shadowAnchor: [10, 10],  // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var wisataIcon = new L.icon({
+  iconUrl: 'http://pulauharapankita.com/img/pulauharapankita.png',
+  shadowUrl: 'http://pulauharapankita.com/img/pulauharapankita.png',
+
+  iconSize:     [20, 20], // size of the icon
+  shadowSize:   [20, 20], // size of the shadow
+  iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+  shadowAnchor: [10, 10],  // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var gedungOlarahragaIcon = new L.icon({
+  iconUrl: 'https://wallazee.global.ssl.fastly.net/images/variant/20130718-ba42c0329945c2c3ee70b73285e150d9ca1f109f1005f2a45586fce-1024.png',
+  shadowUrl: 'https://wallazee.global.ssl.fastly.net/images/variant/20130718-ba42c0329945c2c3ee70b73285e150d9ca1f109f1005f2a45586fce-1024.png',
+
+  iconSize:     [20, 20], // size of the icon
+  shadowSize:   [20, 20], // size of the shadow
+  iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+  shadowAnchor: [10, 10],  // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var hotelIcon = new L.icon({
+  iconUrl: 'https://cdn2.iconfinder.com/data/icons/large-home-icons/512/Hotel_travel_holiday_vector_vacation_symbol.png',
+  shadowUrl: 'https://cdn2.iconfinder.com/data/icons/large-home-icons/512/Hotel_travel_holiday_vector_vacation_symbol.png',
+
+  iconSize:     [20, 20], // size of the icon
+  shadowSize:   [20, 20], // size of the shadow
+  iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+  shadowAnchor: [10, 10],  // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
